02b44f669b93d73651c3cf2470dee3d821ce353f
Hanyue Zhou <alexalexchou@outlook.com>
2017-10-25 02:30:07 +0200

    Initialization of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatSliderModule} from '@angular/material';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
